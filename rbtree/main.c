6da676a56c3f3ccec368dcb552a8a75e44df2d24
kzenayuca <kzenayuca@unsa.edu.pe>
2024-12-03 08:31:57 -0500

    segmentation fault con este arbol en especifico

--- rbtree/main.c
+++ rbtree/main.c
@@ -3,22 +3,26 @@
 
 int main(){
   RBTree* t = createRBTree();
-  arbolEjemplo(t);
+  //arbolEjemplo(t);
   RBNode* x = createRBNode(1);
   RBNode* y = createRBNode(2);
   RBNode* z = createRBNode(4);
   RBNode* w = createRBNode(5);
   RBNode* q = createRBNode(7);
-
-  //rbInsert(t,x);
-  //rbInsert(t,y);
-  //rbInsert(t,z);
-  //rbInsert(t,w);
-  //rbInsert(t,q);
-  //rbInsert(t,createRBNode(8));
-  //rbInsert(t,createRBNode(11));
-  //rbInsert(t,createRBNode(14));
-  //rbInsert(t,createRBNode(15));
+  
+  RBNode* a = createRBNode(8);
+  RBNode* b = createRBNode(11);
+  RBNode* c = createRBNode(14);
+  RBNode* d = createRBNode(15);
+  rbInsert(t,x);
+  rbInsert(t,y);
+  rbInsert(t,z);
+  rbInsert(t,w);
+  rbInsert(t,q);
+  rbInsert(t,a);
+  rbInsert(t,b);
+  rbInsert(t,c);
+  rbInsert(t,d);
 
   int bh = blackHeight(t->root);
   printf("Altura negra: %d \n",bh);
